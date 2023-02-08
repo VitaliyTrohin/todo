d87ad30ffd9db6b1cbebe7052ab4292901e70614
vetal <you@example.com>
2023-02-08 19:23:52 +0300

    Routing

--- frontend/src/components/Author.js
+++ frontend/src/components/Author.js
@@ -1,35 +1,26 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 
-const AuthorItem = ({ author }) => {
+const AuthorItem = ({ item }) => {
     return (
         <tr>
-            <td>
-                {author.first_name}
-            </td>
-            <td>
-                {author.last_name}
-            </td>
-            <td>
-                {author.birthday_year}
-            </td>
+            <td><Link to={`author/${item.id}`}>{item.id}</Link></td>
+            <td>{item.name}</td>
+            <td>{item.birthday_year}</td>
         </tr>
     )
 }
 
-const AuthorList = ({ authors }) => {
+const AuthorList = ({ items }) => {
     return (
         <table>
-            <th>
-                First name
-            </th>
-            <th>
-                Last Name
-            </th>
-            <th>
-                Birthday year
-            </th>
-            {authors.map((author) => <AuthorItem author={author} />)}
+            <tr>
+                <th>ID</th>
+                <th>NAME</th>
+                <th>BIRTHDAY_YEAR</th>
+            </tr>
+            {items.map((item) => <AuthorItem item={item} />)}
         </table>
     )
 }
